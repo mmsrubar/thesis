7367d231412da11f91b97d74c03af345be765f63
Michal Srubar <mmsrubar@gmail.com>
2014-02-22 08:11:24 +0100

    more notes about request and callback private data

--- usefull_code/tevent/async_req.c
+++ usefull_code/tevent/async_req.c
@@ -10,21 +10,27 @@
 #include <tevent.h>
 #include <sys/time.h>
 
+/*
+ * Callback can obtain two kind of data.
+ */
 static void my_print(struct tevent_req *req) {
-	int *event_data = tevent_req_data(req, int);
-	printf("Private data: %d\n", *event_data);
 
+	// get data that are bind with the request event
+	int *event_data = tevent_req_data(req, int);
+	printf("Event's private data:\t\t%d\n", *event_data);
+
+	// get data that was given to the callback 
 	void *c_data = tevent_req_callback_data_void(req);
-	printf("Callback's data: %s\n", (char *) c_data);
+	printf("Callback's private data:\t%s\n", (char *) c_data);
 }
 
 int main(void) {
 	
-	TALLOC_CTX *mem_ctx;								// pointer to talloc context
-	struct tevent_context *ctx;		// pointer to tevent context
-	struct tevent_req *event_req;		// pointer to tevent context
-	int *data;	// pointer to private data
-	char *callback_data = "callback specific data";
+	TALLOC_CTX *mem_ctx;						// pointer to talloc context
+	struct tevent_context *ctx;			// pointer to tevent context
+	struct tevent_req *event_req;		// pointer to tevent request
+	int *event_data;								// pointer to private request data
+	char *callback_data = "callback's data"; // pointer to a data
 
 	// parent - top level talloc context
 	mem_ctx = talloc_new(NULL); 
@@ -35,22 +41,23 @@ int main(void) {
 	
 	ctx = tevent_context_init(mem_ctx);
 
-	// this also allocate memory for private data 'data'
-	event_req = tevent_req_create(mem_ctx, &data, int);
+	// this also allocate memory for private request's data 
+	event_req = tevent_req_create(mem_ctx, &event_data, int);
 	if (event_req == NULL) {
 		fprintf(stderr, "Error while creating tevent request.\n");
 		return -1;
 	}
 
-	// set private data
-	*data = 312;
+	// set private request's data
+	*event_data = 312;
 
 	// bind a callback to a asynchronous request
 	tevent_req_set_callback(event_req, my_print, callback_data);
 
-	// trigger the request?
+	// mark request af finished immediatly
 	//tevent_req_done(event_req);
 
+	// mark request as finished after 1s
 	tevent_req_set_endtime(event_req, ctx, tevent_timeval_current_ofs(1, 0));
 
 	tevent_loop_wait(ctx);
